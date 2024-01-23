6d261fe8acc298b9304760e23a403936cd127fe5
Krystian Grabowy <krys9802@gmail.com>
2024-01-23 17:13:42 +0100

    Initial Commit

---
+++ ToDoSwiftUI-MVVM/ToDoSwiftUI_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoSwiftUI_MVVMApp.swift
+//  ToDoSwiftUI-MVVM
+//
+//  Created by Krystian Grabowy on 23/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoSwiftUI_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
