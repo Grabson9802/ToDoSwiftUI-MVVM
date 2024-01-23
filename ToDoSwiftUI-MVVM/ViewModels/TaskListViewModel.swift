3e3d0f4343c4c5cc2acfea94e46519dfc16a81e4
Krystian Grabowy <krys9802@gmail.com>
2024-01-23 17:25:05 +0100

    saving tasks into user defaults implemented

--- ToDoSwiftUI-MVVM/ViewModels/TaskListViewModel.swift
+++ ToDoSwiftUI-MVVM/ViewModels/TaskListViewModel.swift
@@ -11,11 +11,37 @@ import Combine
 class TaskListViewModel: ObservableObject {
     @Published var tasks: [Task] = []
 
+    init() {
+        loadTasks()
+    }
+
     func addTask(title: String) {
         tasks.append(Task(title: title))
+        saveTasks()
     }
 
     func deleteTask(at indexSet: IndexSet) {
         tasks.remove(atOffsets: indexSet)
+        saveTasks()
+    }
+
+    private func saveTasks() {
+        do {
+            let encodedData = try JSONEncoder().encode(tasks)
+            UserDefaults.standard.set(encodedData, forKey: "tasks")
+        } catch {
+            print("Error encoding tasks: \(error.localizedDescription)")
+        }
+    }
+
+    private func loadTasks() {
+        if let encodedData = UserDefaults.standard.data(forKey: "tasks") {
+            do {
+                tasks = try JSONDecoder().decode([Task].self, from: encodedData)
+            } catch {
+                print("Error decoding tasks: \(error.localizedDescription)")
+            }
+        }
     }
 }
+
